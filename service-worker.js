8fd30f65d2b655713142f92d1a976009b4ee6268
Lee Alexis <labermejo@eml.cc>
2016-05-29 08:59:22 +0800

    heroku: trial and error of serviceworker

--- service-worker.js
+++ service-worker.js
@@ -25,17 +25,40 @@ var filesToCache = [
 
 /* Skips waiting */
 
-self.addEventListener('install', function(e) {
+// self.addEventListener('install', function(e) {
+//   console.log('[ServiceWorker] Install');
+//   e.waitUntil(
+//     caches.open(cacheName)
+//     .then(function(cache) {
+//       console.log('[ServiceWorker] Caching App Shell');
+//       return cache.addAll(filesToCache);      
+//     })
+//     .then(() => self.skipWaiting());
+//   )
+// });
+
+self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(function(cache) {
-      console.log('[ServiceWorker] Caching App Shell');
-      return cache.addAll(filesToCache);      
+  event.waitUntil(
+  caches.open(cacheName).then(function(cache) {
+        // Important to `return` the promise here to have `skipWaiting()`
+        // fire after the cache has been updated.
+        console.log('[ServiceWorker] Caching App Shell');
+        return cache.addAll(filesToCache);
+    }).then(function() {
+      console.log('[ServiceWorker] Skip waiting');
+      // `skipWaiting()` forces the waiting ServiceWorker to become the
+      // active ServiceWorker, triggering the `onactivate` event.
+      // Together with `Clients.claim()` this allows a worker to take effect
+      // immediately in the client(s).
+      return self.skipWaiting();
     })
-    .then(() => self.skipWaiting());
-  )
+  );
 });
+
+
+
+
 
 self.addEventListener('activate',  event => {
   console.log('[ServiceWorker] Activate');
