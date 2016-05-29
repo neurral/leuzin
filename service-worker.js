f6e918abcd6bcdf98ef0a80aaecec9562c42b4d5
Lee Alexis <labermejo@eml.cc>
2016-05-29 08:56:49 +0800

    heroku: trial and error of serviceworker

--- service-worker.js
+++ service-worker.js
@@ -28,11 +28,12 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName)
+    .then(function(cache) {
       console.log('[ServiceWorker] Caching App Shell');
-      return cache.addAll(filesToCache)      
-      .then(() => self.skipWaiting());
+      return cache.addAll(filesToCache);      
     })
+    .then(() => self.skipWaiting());
   )
 });
 
